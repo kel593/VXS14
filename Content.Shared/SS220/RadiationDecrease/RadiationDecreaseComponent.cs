97613c167d20493f91482c39ecbb963538ecd484
aksenV <zendr32@gmail.com>
2024-08-19 21:13:04 +0900

    Ports radiation decrease component

---
+++ Content.Shared/SS220/RadiationDecrease/RadiationDecreaseComponent.cs
@@ -0,0 +1,19 @@
+namespace Content.Shared.SS220.RadiationDecrease;
+
+/// <summary>
+///     Decrease radiation per second in Damaged RTG
+/// </summary>
+[RegisterComponent]
+public sealed partial class RadiationDecreaseComponent : Component
+{
+    public readonly int TotalAliveTime = 1200; // TODO: Make redactable, ~20 minutes
+
+    public readonly TimeSpan CoolDown = TimeSpan.FromSeconds(1f);
+
+    public TimeSpan LastTimeDecreaseRad = TimeSpan.Zero;
+    public TimeSpan LastTimeDecreaseSupply = TimeSpan.Zero;
+
+    public float Intensity = 0; // radiation capacity in RadiationSourceComponent
+    public float Supply = 0; // powersupp in PowerSupplierComponent
+
+}
