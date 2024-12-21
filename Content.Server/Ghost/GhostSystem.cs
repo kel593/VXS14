d8dc6d0f1114a0200d93adc03ffddb5e1685dfe2
aksenV <zendr32@gmail.com>
2024-12-21 20:52:38 +0900

    Working respawn system

--- Content.Server/Ghost/GhostSystem.cs
+++ Content.Server/Ghost/GhostSystem.cs
@@ -579,6 +579,8 @@ namespace Content.Server.Ghost
             if (ghost == null)
                 return false;
 
+            EntityManager.SystemOrNull<Backmen.Ghost.GhostReJoinSystem>()?.AttachGhost(ghost, mind.Session); // backmen: ReturnToRound
+
             return true;
         }
     }
