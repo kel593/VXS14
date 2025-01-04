61b1ba94b9006cceeb0e277d04b05c26a6ba07cd
aksenV <zendr32@gmail.com>
2025-01-05 08:22:13 +0900

    Revert "Reapply "хуй""
    This reverts commit 576109c984b2ff1f8e34689740879ab579722eb7.

--- Content.Server/_VXS14/TriggerOnLand/TriggerOnLandComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Server._VXS.TriggerOnLand
-{
-    [RegisterComponent]
-    public sealed partial class TriggerOnLandComponent : Component
-    {
-        [DataField("Chance")] public float Prob = 1f;
-    }
-}
